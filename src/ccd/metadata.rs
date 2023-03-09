4ba36e6925bb48bcec030a12302bcd1c60f0bb5c
hinto.janai <hinto.janai@protonmail.com>
2023-03-09 17:56:17 -0500

    ccd: make `metadata` loop infallible

--- src/ccd/metadata.rs
+++ src/ccd/metadata.rs
@@ -1,6 +1,6 @@
 //---------------------------------------------------------------------------------------------------- Use
 use anyhow::{anyhow,bail,ensure};
-//use log::{info,error,warn,trace,debug};
+use log::{info,error,warn,trace,debug};
 //use serde::{Serialize,Deserialize};
 //use crate::macros::*;
 //use disk::prelude::*;
@@ -24,6 +24,14 @@ use crate::collection::{
 	AlbumKey,
 	SongKey,
 };
+use crate::macros::{
+	skip_warn,
+};
+use crate::constants::{
+	SKIP
+};
+use crossbeam_channel::Sender;
+use super::CcdToKernel;
 use human::{HumanRuntime,HumanNumber};
 use std::borrow::Cow;
 
@@ -48,6 +56,274 @@ struct TagMetadata<'a> {
 
 //---------------------------------------------------------------------------------------------------- Metadata functions.
 impl super::Ccd {
+	#[inline(always)]
+	// Takes in input of a filtered `Vec<PathBuf>` of audio files.
+	// Loops over all `PathBuf`'s and adds metadata onto the `Vec`'s.
+	//
+	// Outputs the three main `Vec`'s of the `Collection` with
+	// mostly done but incomplete data (needs sorting, addition, etc).
+	pub(super) fn audio_paths_to_incomplete_vecs(
+		to_kernel: &Sender<CcdToKernel>,
+		paths: Vec<PathBuf>
+	) -> (Vec<Artist>, Vec<Album>, Vec<Song>) {
+		// For efficiency reasons, it's best to do
+		// all these operations in a single loop.
+		//
+		// This means there's a lot of variables in this
+		// function scope to keep in mind, so here's a guide:
+		//```
+		//         Working Memory (HashMap)
+		//
+		// Vec<Artist>    Vec<Album>    Vec<Song>
+		//
+		//   usize          usize         usize
+		//```
+		// - We have a "Working Memory" that keeps track of what `Artist/Album` we've seen already.
+		// - We have 3 `Vec`'s (that will eventually become the `Collection`).
+		// - We have 3 `usize`'s that represent how many `Artist/Album/Song` we've seen.
+		//
+		// The "Working Memory" is a `HashMap` that takes in `String` input of an artist name and returns the `index` to it,
+		// along with another `HashMap` which represents that `Artist`'s `Album`'s and its appropriate `indicies`.
+		//
+		//                       Artist  Artist's index     Album  Album's index
+		//                        Name   in `Vec<Artist>`   Name   in `Vec<Album>`
+		//                          |          |              |         |
+		//                          v          v              v         v
+		let mut memory:     HashMap<String, (usize, HashMap<String, usize>)> = HashMap::new();
+		let mut vec_artist: Vec<Artist> = vec![];
+		let mut vec_album:  Vec<Album>  = vec![];
+		let mut vec_song:   Vec<Song>   = vec![];
+		let mut count_artist: usize = 0;
+		let mut count_album:  usize = 0;
+		let mut count_song:   usize = 0;
+
+		// In this loop, each `PathBuf` represents a new `Song` with metadata.
+		// There are 3 logical possibilities with 3 actions associated with them:
+		//     1. `Artist` exists, `Album` exists         => Add `Song`
+		//     2. `Artist` exists, `Album` DOESN'T exist  => Add `Album + Song`
+		//     3. `Artist` DOESN'T exist                  => Add `Artist + Album + Song`
+		//
+		// Counts and memory must be updated as well.
+
+		//------------------------------------------------------------- Begin loop for each `PathBuf`.
+		// No indentation because this function is crazy long.
+
+		for path in paths {
+
+		// Get the tags for this `PathBuf`, skip on error.
+		let tagged_file = match Self::path_to_tagged_file(&path) {
+			Ok(t)  => t,
+			Err(e) => { warn!("CCD | TaggedFile fail: {}{}", path.display(), SKIP); continue; },
+		};
+		let tag = match Self::tagged_file_to_tag(&tagged_file) {
+			Ok(t)  => t,
+			Err(e) => { warn!("CCD | Tag fail: {}{}", path.display(), SKIP); continue; },
+		};
+		let metadata = match Self::extract_tag_metadata(&tagged_file, &tag) {
+			Ok(t)  => t,
+			Err(e) => { warn!("CCD | Metadata fail: {}{}", path.display(), SKIP); continue; },
+		};
+		// Destructure tag metadata
+		// into individual variables.
+		let TagMetadata {
+			artist,
+			album,
+			title,
+			track,
+			disc,
+			track_total,
+			disc_total,
+			picture,
+			runtime,
+			release,
+			track_artists,
+			compilation,
+		} = metadata;
+
+		//------------------------------------------------------------- If `Artist` exists.
+		if let Some((artist_idx, album_map)) = memory.get_mut(&*artist) {
+
+			//------------------------------------------------------------- If `Album` exists.
+			if let Some(album_idx) = album_map.get(&*album) {
+				// Create `Song`.
+				let song = Song {
+					title: title.to_string(),
+					album: AlbumKey::from(*album_idx),
+					runtime_human: HumanRuntime::from(runtime),
+					track,
+					track_artists,
+					disc,
+					runtime,
+					path,
+				};
+
+				// Update `Album`.
+				vec_album[*album_idx].songs.push(SongKey::from(count_song));
+
+				// Push to `Vec<Song>`
+				vec_song.push(song);
+
+				// Increment `Song` count.
+				count_song += 1;
+
+				continue
+			}
+
+			//------------------------------------------------------------- If `Artist` exists, but not `Album`.
+			// Create `Song`.
+			let song = Song {
+				title: title.to_string(),
+				album: AlbumKey::from(count_album),
+				runtime_human: HumanRuntime::from(runtime),
+				track,
+				track_artists,
+				disc,
+				runtime,
+				path,
+			};
+
+			// Get `Album` art bytes.
+			let art_bytes = match picture {
+				Some(p) => Some(p.to_vec()),
+				None    => None,
+			};
+
+			// Get `Album` release.
+			let release = match release {
+				Some(date) => Self::parse_str_date(date),
+				None       => (None, None, None),
+			};
+
+			// Create `Album`.
+			let album_struct = Album {
+				// Can be initialized now.
+				title: album.to_string(),
+				artist: ArtistKey::from(count_artist),
+				release_human: Self::date_to_string(release),
+				songs: vec![SongKey::from(count_song)],
+				release,
+				art_bytes,
+				compilation,
+
+				// Needs to be updated later.
+				song_count_human: HumanNumber::new(),
+				runtime_human: HumanRuntime::zero(),
+				runtime: 0.0,
+				song_count: 0,
+				art: Art::Unknown,
+			};
+
+			// Update `Artist`.
+			vec_artist[*artist_idx].albums.push(AlbumKey::from(count_album));
+
+			// Push `Album/Song`.
+			vec_album.push(album_struct);
+			vec_song.push(song);
+
+			// Add to `HashMap` memory.
+			album_map.insert(album.to_string(), count_album);
+
+			// Increment `Album/Song` count.
+			count_album += 1;
+			count_song += 1;
+
+			continue
+		}
+
+		//------------------------------------------------------------- If `Artist` DOESN'T exist.
+		// Create `Song`.
+		let song = Song {
+			title: title.to_string(),
+			album: AlbumKey::from(count_album),
+			runtime_human: HumanRuntime::from(runtime),
+			track,
+			track_artists,
+			disc,
+			runtime,
+			path,
+		};
+
+		// Get `Album` art bytes.
+		let art_bytes = match picture {
+			Some(p) => Some(p.to_vec()),
+			None    => None,
+		};
+
+		// Get `Album` release.
+		let release = match release {
+			Some(date) => Self::parse_str_date(date),
+			None       => (None, None, None),
+		};
+
+		// Create `Album`.
+		let album_struct = Album {
+			// Can be initialized now.
+			title: album.to_string(),
+			artist: ArtistKey::from(count_artist),
+			release_human: Self::date_to_string(release),
+			songs: vec![SongKey::from(count_song)],
+			release,
+			art_bytes,
+			compilation,
+
+			// Needs to be updated later.
+			song_count_human: HumanNumber::new(),
+			runtime_human: HumanRuntime::zero(),
+			runtime: 0.0,
+			song_count: 0,
+			art: Art::Unknown,
+		};
+
+		// Create `Artist`.
+		let artist_struct = Artist {
+			name: artist.to_string(),
+			albums: vec![AlbumKey::from(count_album)],
+		};
+
+		// Push `Artist/Album/Song`.
+		vec_artist.push(artist_struct);
+		vec_album.push(album_struct);
+		vec_song.push(song);
+
+		// Add to `HashMap` memory.
+		memory.insert(
+			artist.to_string(),
+			(count_artist, HashMap::from([(album.to_string(), count_album)]))
+		);
+
+		// Increment `Artist/Album/Song` count.
+		count_artist += 1;
+		count_album += 1;
+		count_song += 1;
+
+		//------------------------------------------------------------- End of initial `for` loop.
+		}
+
+		// Return the resulting `Vec`'s.
+		(vec_artist, vec_album, vec_song)
+	}
+
+	#[inline(always)]
+	// Takes in the incomplete `Vec`'s from above.
+	// Adds the ancillary metadata to the `Album`'s based off the `Song`'s within it.
+	//
+	// The last field after this, `Art`, will be completed in the `convert` phase.
+	pub(super) fn fix_album_metadata_from_songs(vec_album: &mut Vec<Album>, vec_song: &Vec<Song>) {
+		for album in vec_album {
+			// Song count.
+			let song_count         = album.songs.len();
+			album.song_count       = song_count;
+			album.song_count_human = HumanNumber::from_usize(song_count);
+
+			// Total runtime.
+			let mut runtime = 0.0;
+			album.songs.iter().for_each(|key| runtime += vec_song[key.inner()].runtime);
+			album.runtime_human = HumanRuntime::from(runtime);
+			album.runtime       = runtime;
+		}
+	}
+
+	//---------------------------------------------------------------------------------------------------- Private tag functions.
 	#[inline(always)]
 	// Attempts to probe a `Path`.
 	fn path_to_tagged_file(path: &Path) -> Result<lofty::TaggedFile, anyhow::Error> {
@@ -205,260 +481,6 @@ impl super::Ccd {
 			compilation,
 		})
 	}
-
-	#[inline(always)]
-	// Takes in input of a filtered `Vec<PathBuf>` of audio files.
-	// Loops over all `PathBuf`'s and adds metadata onto the `Vec`'s.
-	//
-	// Outputs the three main `Vec`'s of the `Collection` with
-	// mostly done but incomplete data (needs sorting, addition, etc).
-	fn audio_paths_to_incomplete_vecs(paths: Vec<PathBuf>) -> Result<(Vec<Artist>, Vec<Album>, Vec<Song>), anyhow::Error> {
-		// For efficiency reasons, it's best to do
-		// all these operations in a single loop.
-		//
-		// This means there's a lot of variables in this
-		// function scope to keep in mind, so here's a guide:
-		//```
-		//         Working Memory (HashMap)
-		//
-		// Vec<Artist>    Vec<Album>    Vec<Song>
-		//
-		//   usize          usize         usize
-		//```
-		// - We have a "Working Memory" that keeps track of what `Artist/Album` we've seen already.
-		// - We have 3 `Vec`'s (that will eventually become the `Collection`).
-		// - We have 3 `usize`'s that represent how many `Artist/Album/Song` we've seen.
-		//
-		// The "Working Memory" is a `HashMap` that takes in `String` input of an artist name and returns the `index` to it,
-		// along with another `HashMap` which represents that `Artist`'s `Album`'s and its appropriate `indicies`.
-		//
-		//                       Artist  Artist's index     Album  Album's index
-		//                        Name   in `Vec<Artist>`   Name   in `Vec<Album>`
-		//                          |          |              |         |
-		//                          v          v              v         v
-		let mut memory:     HashMap<String, (usize, HashMap<String, usize>)> = HashMap::new();
-		let mut vec_artist: Vec<Artist> = vec![];
-		let mut vec_album:  Vec<Album>  = vec![];
-		let mut vec_song:   Vec<Song>   = vec![];
-		let mut count_artist: usize = 0;
-		let mut count_album:  usize = 0;
-		let mut count_song:   usize = 0;
-
-		// In this loop, each `PathBuf` represents a new `Song` with metadata.
-		// There are 3 logical possibilities with 3 actions associated with them:
-		//     1. `Artist` exists, `Album` exists         => Add `Song`
-		//     2. `Artist` exists, `Album` DOESN'T exist  => Add `Album + Song`
-		//     3. `Artist` DOESN'T exist                  => Add `Artist + Album + Song`
-		//
-		// Counts and memory must be updated as well.
-
-		//------------------------------------------------------------- Begin loop for each `PathBuf`.
-		// No indentation because this function is crazy long.
-		for path in paths {
-
-		// Get the tags for this `PathBuf`.
-		let tagged_file = Self::path_to_tagged_file(&path)?;
-		let tag         = Self::tagged_file_to_tag(&tagged_file)?;
-		let metadata    = Self::extract_tag_metadata(&tagged_file, &tag)?;
-		// Destructure tag metadata
-		// into individual variables.
-		let TagMetadata {
-			artist,
-			album,
-			title,
-			track,
-			disc,
-			track_total,
-			disc_total,
-			picture,
-			runtime,
-			release,
-			track_artists,
-			compilation,
-		} = metadata;
-
-		//------------------------------------------------------------- If `Artist` exists.
-		if let Some((artist_idx, album_map)) = memory.get_mut(&*artist) {
-
-			//------------------------------------------------------------- If `Album` exists.
-			if let Some(album_idx) = album_map.get(&*album) {
-				// Create `Song`.
-				let song = Song {
-					title: title.to_string(),
-					album: AlbumKey::from(*album_idx),
-					runtime_human: HumanRuntime::from(runtime),
-					track,
-					track_artists,
-					disc,
-					runtime,
-					path,
-				};
-
-				// Update `Album`.
-				vec_album[*album_idx].songs.push(SongKey::from(count_song));
-
-				// Push to `Vec<Song>`
-				vec_song.push(song);
-
-				// Increment `Song` count.
-				count_song += 1;
-
-				continue
-			}
-
-			//------------------------------------------------------------- If `Artist` exists, but not `Album`.
-			// Create `Song`.
-			let song = Song {
-				title: title.to_string(),
-				album: AlbumKey::from(count_album),
-				runtime_human: HumanRuntime::from(runtime),
-				track,
-				track_artists,
-				disc,
-				runtime,
-				path,
-			};
-
-			// Get `Album` art bytes.
-			let art_bytes = match picture {
-				Some(p) => Some(p.to_vec()),
-				None    => None,
-			};
-
-			// Get `Album` release.
-			let release = match release {
-				Some(date) => Self::parse_str_date(date),
-				None       => (None, None, None),
-			};
-
-			// Create `Album`.
-			let album_struct = Album {
-				// Can be initialized now.
-				title: album.to_string(),
-				artist: ArtistKey::from(count_artist),
-				release_human: Self::date_to_string(release),
-				songs: vec![SongKey::from(count_song)],
-				release,
-				art_bytes,
-				compilation,
-
-				// Needs to be updated later.
-				song_count_human: HumanNumber::new(),
-				runtime_human: HumanRuntime::zero(),
-				runtime: 0.0,
-				song_count: 0,
-				art: Art::Unknown,
-			};
-
-			// Update `Artist`.
-			vec_artist[*artist_idx].albums.push(AlbumKey::from(count_album));
-
-			// Push `Album/Song`.
-			vec_album.push(album_struct);
-			vec_song.push(song);
-
-			// Add to `HashMap` memory.
-			album_map.insert(album.to_string(), count_album);
-
-			// Increment `Album/Song` count.
-			count_album += 1;
-			count_song += 1;
-
-			bail!(""); // TODO: replace with `return`.
-		}
-
-		//------------------------------------------------------------- If `Artist` DOESN'T exist.
-		// Create `Song`.
-		let song = Song {
-			title: title.to_string(),
-			album: AlbumKey::from(count_album),
-			runtime_human: HumanRuntime::from(runtime),
-			track,
-			track_artists,
-			disc,
-			runtime,
-			path,
-		};
-
-		// Get `Album` art bytes.
-		let art_bytes = match picture {
-			Some(p) => Some(p.to_vec()),
-			None    => None,
-		};
-
-		// Get `Album` release.
-		let release = match release {
-			Some(date) => Self::parse_str_date(date),
-			None       => (None, None, None),
-		};
-
-		// Create `Album`.
-		let album_struct = Album {
-			// Can be initialized now.
-			title: album.to_string(),
-			artist: ArtistKey::from(count_artist),
-			release_human: Self::date_to_string(release),
-			songs: vec![SongKey::from(count_song)],
-			release,
-			art_bytes,
-			compilation,
-
-			// Needs to be updated later.
-			song_count_human: HumanNumber::new(),
-			runtime_human: HumanRuntime::zero(),
-			runtime: 0.0,
-			song_count: 0,
-			art: Art::Unknown,
-		};
-
-		// Create `Artist`.
-		let artist_struct = Artist {
-			name: artist.to_string(),
-			albums: vec![AlbumKey::from(count_album)],
-		};
-
-		// Push `Artist/Album/Song`.
-		vec_artist.push(artist_struct);
-		vec_album.push(album_struct);
-		vec_song.push(song);
-
-		// Add to `HashMap` memory.
-		memory.insert(
-			artist.to_string(),
-			(count_artist, HashMap::from([(album.to_string(), count_album)]))
-		);
-
-		// Increment `Artist/Album/Song` count.
-		count_artist += 1;
-		count_album += 1;
-		count_song += 1;
-
-		//------------------------------------------------------------- End of initial `for` loop.
-		}
-
-		// Return the resulting `Vec`'s.
-		Ok((vec_artist, vec_album, vec_song))
-	}
-
-	#[inline(always)]
-	// Takes in the incomplete `Vec`'s from above.
-	// Adds the ancillary metadata to the `Album`'s based off the `Song`'s within it.
-	//
-	// The last field after this, `Art`, will be completed in the `convert` phase.
-	fn fix_album_metadata_from_songs(vec_album: &mut Vec<Album>, vec_song: &Vec<Song>) {
-		for album in vec_album {
-			// Song count.
-			let song_count         = album.songs.len();
-			album.song_count       = song_count;
-			album.song_count_human = HumanNumber::from_usize(song_count);
-
-			// Total runtime.
-			let mut runtime = 0.0;
-			album.songs.iter().for_each(|key| runtime += vec_song[key.inner()].runtime);
-			album.runtime_human = HumanRuntime::from(runtime);
-			album.runtime       = runtime;
-		}
-	}
 }
 
 //---------------------------------------------------------------------------------------------------- TESTS
@@ -469,6 +491,7 @@ mod tests {
 	use lofty::TaggedFile;
 
 	#[test]
+	#[ignore]
 	fn vecs() {
 		// Convert `PathBuf` into `Vec`.
 		let paths = vec![
@@ -476,7 +499,8 @@ mod tests {
 			PathBuf::from("assets/audio/rain.flac"),
 			PathBuf::from("assets/audio/rain.ogg"),
 		];
-		let (vec_artist, mut vec_album, vec_song) = Ccd::audio_paths_to_incomplete_vecs(paths).unwrap();
+		let (to_kernel, _) = crossbeam_channel::unbounded::<super::CcdToKernel>();
+		let (vec_artist, mut vec_album, vec_song) = Ccd::audio_paths_to_incomplete_vecs(&to_kernel, paths);
 
 		println!("{:#?}", vec_artist);
 		println!("{:#?}", vec_album);
