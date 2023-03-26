ef85028614856a4b587f9990fc9088036f895311
hinto.janai <hinto.janai@protonmail.com>
2023-03-26 13:11:31 -0400

    gui: state holds current `Key`'s

--- festival-gui/src/state.rs
+++ festival-gui/src/state.rs
@@ -16,6 +16,9 @@ use shukusai::{
 	FESTIVAL,
 	FESTIVAL_HEADER,
 };
+use shukusai::key::{
+	AlbumKey,
+};
 
 //---------------------------------------------------------------------------------------------------- State
 /// `GUI`'s State.
@@ -23,14 +26,38 @@ use shukusai::{
 /// Holds user-mutable `GUI` state.
 bincode_file!(State, Dir::Data, FESTIVAL, "gui", "state", FESTIVAL_HEADER, STATE_VERSION);
 #[derive(Clone,Debug,Default,PartialEq,PartialOrd,Serialize,Deserialize)]
-pub(super) struct State {
+pub struct State {
 	/// Which [`Tab`] are currently on?
-	pub(super) tab: Tab,
+	pub tab: Tab,
+
+	/// Which [`Key`] are we currently on?
+	///
+	/// This means which [`Song`] of what [`Album`]
+	/// of what [`Artist`] are we currently listening to.
+	///
+	/// This acts as a local cache so we don't have to lock
+	/// `KernelState` everytime we want to read the value.
+	///
+	/// [`Option::None`] indicates we aren't listening to anything right now.
+	pub key: Option<Key>,
+
+	/// Which [`Album`] are we on in the `Album` tab?
+	///
+	/// This doesn't necessarily mean we're listening to _this_
+	/// [`Album`], but rather, it means _this_ is the [`Album`]
+	/// that the user is looking at right now.
+	///
+	/// [`Option::None`] indicates we aren't looking at
+	/// any [`Album`] and are in the full [`Album`] art view.
+	pub album: Option<AlbumKey>,
 }
 
 impl State {
-	pub(super) fn new() -> Self {
+	/// Creates a mostly empty [`State`].
+	pub fn new() -> Self {
 		Self {
+			key: None,
+			album: None,
 			..Default::default()
 		}
 	}
