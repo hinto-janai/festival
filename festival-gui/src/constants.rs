dd6fd9c5d557206e968ddb4de3049228138507c1
hinto.janai <hinto.janai@protonmail.com>
2023-03-30 16:26:32 -0400

    gui: add colors to `constants.rs`

--- festival-gui/src/constants.rs
+++ festival-gui/src/constants.rs
@@ -25,6 +25,18 @@ pub const FONT_JULIAMONO:      &[u8] = include_bytes!("../../assets/fonts/JuliaM
 //---------------------------------------------------------------------------------------------------- Icon
 pub const ICON: &[u8] = include_bytes!("../../assets/images/icon/512.png");
 
+//---------------------------------------------------------------------------------------------------- Color
+pub const RED:           egui::Color32 = egui::Color32::from_rgb(230, 50, 50);
+pub const GREEN:         egui::Color32 = egui::Color32::from_rgb(100, 230, 100);
+pub const YELLOW:        egui::Color32 = egui::Color32::from_rgb(230, 230, 100);
+pub const BRIGHT_YELLOW: egui::Color32 = egui::Color32::from_rgb(250, 250, 100);
+pub const BONE:          egui::Color32 = egui::Color32::from_rgb(190, 190, 190); // In between LIGHT_GRAY <-> GRAY
+pub const WHITE:         egui::Color32 = egui::Color32::WHITE;
+pub const GRAY:          egui::Color32 = egui::Color32::GRAY;
+pub const LIGHT_GRAY:    egui::Color32 = egui::Color32::LIGHT_GRAY;
+pub const BLACK:         egui::Color32 = egui::Color32::BLACK;
+pub const DARK_GRAY:     egui::Color32 = egui::Color32::from_rgb(18, 18, 18);
+
 //---------------------------------------------------------------------------------------------------- TESTS
 //#[cfg(test)]
 //mod tests {
