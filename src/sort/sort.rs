760ac12248bf4a77ab986b4bf42f3daf51739b6b
hinto.janai <hinto.janai@protonmail.com>
2023-05-17 18:06:54 -0400

    sort: fix docs

--- src/sort/sort.rs
+++ src/sort/sort.rs
@@ -12,9 +12,9 @@ use crate::collection::{
 
 //---------------------------------------------------------------------------------------------------- Sort Constants
 /// [`ArtistSort::Lexi`]
-pub const ARTIST_LEXI:                    &str = "Artists lexicographically (A-Z)";
+pub const ARTIST_LEXI:                    &str = "Artists A-Z";
 /// [`ArtistSort::LexiRev`]
-pub const ARTIST_LEXI_REV:                &str = "Artists lexicographically (Z-A)";
+pub const ARTIST_LEXI_REV:                &str = "Artists Z-A";
 /// [`ArtistSort::AlbumCount`]
 pub const ARTIST_ALBUM_COUNT:             &str = "Artists per album count (least to most)";
 /// [`ArtistSort::AlbumCountRev`]
@@ -25,46 +25,46 @@ pub const ARTIST_SONG_COUNT:              &str = "Artists per song count (least 
 pub const ARTIST_SONG_COUNT_REV:          &str = "Artists per song count (most to least)";
 
 /// [`AlbumSort::ReleaseArtistLexi`]
-pub const ALBUM_RELEASE_ARTIST_LEXI:      &str = "Artists lexicographically (A-Z), albums in release order";
+pub const ALBUM_RELEASE_ARTIST_LEXI:      &str = "Artists A-Z, albums oldest-latest";
 /// [`AlbumSort::ReleaseArtistLexiRev`]
-pub const ALBUM_RELEASE_ARTIST_LEXI_REV:  &str = "Artists lexicographically (Z-A), albums in release order";
+pub const ALBUM_RELEASE_ARTIST_LEXI_REV:  &str = "Artists Z-A, albums oldest-latest";
 /// [`AlbumSort::LexiArtistLexi`]
-pub const ALBUM_LEXI_ARTIST_LEXI:         &str = "Artists lexicographically (A-Z), albums lexicographically";
+pub const ALBUM_LEXI_ARTIST_LEXI:         &str = "Artists A-Z, albums A-Z";
 /// [`AlbumSort::LexiArtistLexiRev`]
-pub const ALBUM_LEXI_ARTIST_LEXI_REV:     &str = "Artists lexicographically (Z-A), albums lexicographically";
+pub const ALBUM_LEXI_ARTIST_LEXI_REV:     &str = "Artists Z-A, albums A-Z";
 /// [`AlbumSort::Lexi`]
-pub const ALBUM_LEXI:                     &str = "Albums lexicographically (A-Z)";
+pub const ALBUM_LEXI:                     &str = "Albums A-Z";
 /// [`AlbumSort::LexiRev`]
-pub const ALBUM_LEXI_REV:                 &str = "Albums lexicographically (Z-A)";
+pub const ALBUM_LEXI_REV:                 &str = "Albums Z-A";
 /// [`AlbumSort::Release`]
-pub const ALBUM_RELEASE:                  &str = "Albums in release order (oldest to latest)";
+pub const ALBUM_RELEASE:                  &str = "Albums oldest-latest";
 /// [`AlbumSort::ReleaseRev`]
-pub const ALBUM_RELEASE_REV:              &str = "Albums in release order (latest to oldest)";
+pub const ALBUM_RELEASE_REV:              &str = "Albums oldest-latest";
 /// [`AlbumSort::Runtime`]
-pub const ALBUM_RUNTIME:                  &str = "Albums shortest to longest";
+pub const ALBUM_RUNTIME:                  &str = "Albums shortest-longest";
 /// [`AlbumSort::RuntimeRev`]
-pub const ALBUM_RUNTIME_REV:              &str = "Albums longest to shortest";
+pub const ALBUM_RUNTIME_REV:              &str = "Albums longest-shortest";
 
 /// [`SongSort::AlbumReleaseArtistLexi`]
-pub const SONG_ALBUM_RELEASE_ARTIST_LEXI:     &str = "Artists lexicographically (A-Z), albums in release order, songs in track order";
+pub const SONG_ALBUM_RELEASE_ARTIST_LEXI:     &str = "Artists A-Z, albums oldest-latest, songs in track order";
 /// [`SongSort::AlbumReleaseArtistLexiRev`]
-pub const SONG_ALBUM_RELEASE_ARTIST_LEXI_REV: &str = "Artists lexicographically (Z-A), albums in release order, songs in track order";
+pub const SONG_ALBUM_RELEASE_ARTIST_LEXI_REV: &str = "Artists Z-A, albums oldest-latest, songs in track order";
 /// [`SongSort::AlbumLexiArtistLexi`]
-pub const SONG_ALBUM_LEXI_ARTIST_LEXI:        &str = "Artists lexicographically (A-Z), albums lexicographically,, songs in track order";
+pub const SONG_ALBUM_LEXI_ARTIST_LEXI:        &str = "Artists A-Z, albums A-Z, songs in track order";
 /// [`SongSort::AlbumLexiArtistLexiRev`]
-pub const SONG_ALBUM_LEXI_ARTIST_LEXI_REV:    &str = "Artists lexicographically (Z-A), albums lexicographically,, songs in track order";
+pub const SONG_ALBUM_LEXI_ARTIST_LEXI_REV:    &str = "Artists Z-A, albums A-Z, songs in track order";
 /// [`SongSort::Lexi`]
-pub const SONG_LEXI:                          &str = "Songs lexicographically (A-Z)";
+pub const SONG_LEXI:                          &str = "Songs A-Z";
 /// [`SongSort::Lexi`]
-pub const SONG_LEXI_REV:                      &str = "Songs lexicographically (Z-A)";
+pub const SONG_LEXI_REV:                      &str = "Songs Z-A";
 /// [`SongSort::Release`]
-pub const SONG_RELEASE:                       &str = "Songs in release order (oldest to latest)";
+pub const SONG_RELEASE:                       &str = "Songs oldest-latest";
 /// [`SongSort::ReleaseRev`]
-pub const SONG_RELEASE_REV:                   &str = "Songs in release order (latest to oldest)";
+pub const SONG_RELEASE_REV:                   &str = "Songs oldest-latest";
 /// [`SongSort::Runtime`]
-pub const SONG_RUNTIME:                       &str = "Songs shortest to longest";
+pub const SONG_RUNTIME:                       &str = "Songs shortest-longest";
 /// [`SongSort::RuntimeRev`]
-pub const SONG_RUNTIME_REV:                   &str = "Songs longest to oldest";
+pub const SONG_RUNTIME_REV:                   &str = "Songs longest-oldest";
 
 //---------------------------------------------------------------------------------------------------- Sort
 /// All the ways to sort the [`Collection`]'s [`Artist`]'s.
@@ -74,18 +74,18 @@ pub const SONG_RUNTIME_REV:                   &str = "Songs longest to oldest";
 /// `lexi` is shorthand for `lexicographically`.
 #[derive(Copy,Clone,Debug,Default,Hash,PartialEq,Eq,PartialOrd,Ord,Serialize,Deserialize,Encode,Decode)]
 pub enum ArtistSort {
-	/// [`Artist`] in `lexi`. Field: [`Collection::sort_artist_lexi`].
+	/// [`Artist`] A-Z. Field: [`Collection::sort_artist_lexi`].
 	Lexi,
-	/// [`Artist`] in `lexi`. Field: [`Collection::sort_artist_lexi`] (reversed).
+	/// [`Artist`] Z-A. Field: [`Collection::sort_artist_lexi_rev`].
 	LexiRev,
 	/// [`Artist`] with most `Album`'s to least. Field: [`Collection::sort_artist_album_count`].
 	AlbumCount,
 	#[default]
-	/// [`Artist`] with most `Album`'s to least. Field: [`Collection::sort_artist_album_count`] (reversed).
+	/// [`Artist`] with least `Album`'s to most. Field: [`Collection::sort_artist_album_count_rev`].
 	AlbumCountRev,
 	/// [`Artist`] with most `Song`'s to least. Field: [`Collection::sort_artist_song_count`].
 	SongCount,
-	/// [`Artist`] with most `Song`'s to least. Field: [`Collection::sort_artist_song_count`] (reversed).
+	/// [`Artist`] with least `Song`'s to most. Field: [`Collection::sort_artist_song_count_rev`].
 	SongCountRev,
 }
 
@@ -97,25 +97,25 @@ pub enum ArtistSort {
 #[derive(Copy,Clone,Debug,Default,Hash,PartialEq,Eq,PartialOrd,Ord,Serialize,Deserialize,Encode,Decode)]
 pub enum AlbumSort {
 	#[default]
-	/// [`Artist`] `lexi`, [`Album`]'s oldest release to latest. Field: [`Collection::sort_album_release_artist_lexi`].
+	/// [`Artist`] A-Z, [`Album`] oldest-latest. Field: [`Collection::sort_album_release_artist_lexi`].
 	ReleaseArtistLexi,
-	/// [`Artist`] `lexi`, [`Album`]'s oldest release to latest. Field: [`Collection::sort_album_release_artist_lexi`] (reversed).
+	/// [`Artist`] Z-A, [`Album`] oldest-latest. Field: [`Collection::sort_album_release_artist_lexi_rev`].
 	ReleaseArtistLexiRev,
-	/// [`Artist`] `lexi`, [`Album`]'s `lexi`. Field: [`Collection::sort_album_lexi_artist_lexi`].
+	/// [`Artist`] A-Z, [`Album`]'s A-Z. Field: [`Collection::sort_album_lexi_artist_lexi`].
 	LexiArtistLexi,
-	/// [`Artist`] `lexi`, [`Album`]'s `lexi`. Field: [`Collection::sort_album_lexi_artist_lexi`] (reversed).
+	/// [`Artist`] Z-A, [`Album`]'s A-Z. Field: [`Collection::sort_album_lexi_artist_lexi_rev`].
 	LexiArtistLexiRev,
-	/// [`Album`] lexi. Field: [`Collection::sort_album_lexi`].
+	/// [`Album`] A-Z. Field: [`Collection::sort_album_lexi`].
 	Lexi,
-	/// [`Album`] lexi. Field: [`Collection::sort_album_lexi`] (reversed).
+	/// [`Album`] Z-A. Field: [`Collection::sort_album_lexi_rev`].
 	LexiRev,
-	/// [`Album`] oldest to latest. Field: [`Collection::sort_album_release`].
+	/// [`Album`] oldest-latest. Field: [`Collection::sort_album_release`].
 	Release,
-	/// [`Album`] latest to oldest. Field: [`Collection::sort_album_release`].
+	/// [`Album`] latest-oldest. Field: [`Collection::sort_album_release_rev`].
 	ReleaseRev,
-	/// [`Album`] shortest to longest. Field: [`Collection::sort_album_runtime`].
+	/// [`Album`] shortest-longest. Field: [`Collection::sort_album_runtime`].
 	Runtime,
-	/// [`Album`] longest to shortest. Field: [`Collection::sort_album_runtime`].
+	/// [`Album`] longest-shortest. Field: [`Collection::sort_album_runtime_rev`].
 	RuntimeRev,
 }
 
@@ -126,26 +126,26 @@ pub enum AlbumSort {
 /// `lexi` is shorthand for `lexicographically`.
 #[derive(Copy,Clone,Debug,Default,Hash,PartialEq,Eq,PartialOrd,Ord,Serialize,Deserialize,Encode,Decode)]
 pub enum SongSort {
-	/// [`Artist`] lexi, [`Album`] release, [`Song`] track_number. Field: [`Collection::sort_song_album_release_artist_lexi`].
+	/// [`Artist`] A-Z, [`Album`] oldest-latest, [`Song`] track_number. Field: [`Collection::sort_song_album_release_artist_lexi`].
 	AlbumReleaseArtistLexi,
-	/// [`Artist`] lexi, [`Album`] release, [`Song`] track_number. Field: [`Collection::sort_song_album_release_artist_lexi`] (reversed).
+	/// [`Artist`] Z-A, [`Album`] oldest-latest, [`Song`] track_number. Field: [`Collection::sort_song_album_release_artist_lexi_rev`].
 	AlbumReleaseArtistLexiRev,
-	/// [`Artist`] lexi, [`Album`] lexi, [`Song`] track_number. Field: [`Collection::sort_song_album_lexi_artist_lexi`].
+	/// [`Artist`] A-Z, [`Album`] A-Z, [`Song`] track_number. Field: [`Collection::sort_song_album_lexi_artist_lexi`].
 	AlbumLexiArtistLexi,
-	/// [`Artist`] lexi, [`Album`] lexi, [`Song`] track_number. Field: [`Collection::sort_song_album_lexi_artist_lexi`] (reversed).
+	/// [`Artist`] Z-A, [`Album`] A-Z, [`Song`] track_number. Field: [`Collection::sort_song_album_lexi_artist_lexi_rev`].
 	AlbumLexiArtistLexiRev,
-	/// [`Song`] lexi. Field: [`Collection::sort_song_lexi`].
+	/// [`Song`] A-Z. Field: [`Collection::sort_song_lexi`].
 	#[default]
 	Lexi,
-	/// [`Song`] lexi. Field: [`Collection::sort_song_lexi`] (reversed).
+	/// [`Song`] Z-A. Field: [`Collection::sort_song_lexi_rev`].
 	LexiRev,
-	/// [`Song`] oldest to latest. Field: [`Collection::sort_song_release`].
+	/// [`Song`] oldest-latest. Field: [`Collection::sort_song_release`].
 	Release,
-	/// [`Song`] latest to oldest. Field: [`Collection::sort_song_release`].
+	/// [`Song`] latest-oldest. Field: [`Collection::sort_song_release`].
 	ReleaseRev,
-	/// [`Song`] shortest to longest. Field: [`Collection::sort_song_runtime`].
+	/// [`Song`] shortest-longest. Field: [`Collection::sort_song_runtime`].
 	Runtime,
-	/// [`Song`] longest to shortest. Field: [`Collection::sort_song_runtime`].
+	/// [`Song`] longest-shortest. Field: [`Collection::sort_song_runtime`].
 	RuntimeRev,
 }
 
